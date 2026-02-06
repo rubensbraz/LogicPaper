5c0df3300e8a6d9dcfbf1c700a3d5702e1587f95
Rubens  Braz <rubensbrazf@gmail.com>
2026-02-07 01:45:54 +0900

    Update navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -70,10 +70,12 @@ function renderNavbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
                         <span>${i18n.t('navbar.link_help')}</span>
                     </a>
+                    ${!CONFIG.env.isGithubPages ? `
                     <a href="/docs" target="_blank" class="${inactiveClass} flex items-center justify-center gap-2">
                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><line x1="10" y1="9" x2="8" y2="9"/></svg>
                         <span>${i18n.t('navbar.link_api')}</span>
                     </a>
+                    ` : ''}
                     ${toggleLangHTML}
                 </div>
                 <div class="w-full md:w-auto flex justify-center">
